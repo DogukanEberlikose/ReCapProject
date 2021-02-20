37505e896acb2a7d878e51644eed2dbbfc1d29a0
Dogukan Eberlikose <dogukan.eberlikose@gmail.com>
2021-02-20 21:32:13 +0300

    20.02

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            CarManager carManager = new CarManager(new EfCarDal());
+            CarManager carManager = new CarManager(new EfCarDal()); 
             foreach (var car in carManager.GetCarDetails())
             {
                 Console.WriteLine(car.BrandName + "/" + car.ModelYear + "/" + car.Description + "/" + car.DailyPrice );
